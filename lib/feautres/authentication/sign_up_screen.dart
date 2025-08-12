a5ab729551c501e3ea4b5dfaeadd45040321f0d8
romanus <romanusdev@gmail.com>
2025-08-12 21:35:21 +0900

    #login_screen+button

--- lib/feautres/authentication/sign_up_screen.dart
+++ lib/feautres/authentication/sign_up_screen.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/feautres/authentication/login_screen.dart';
+import 'package:tiktok_clone/feautres/authentication/widgets/auth_button.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
+
+  void onLoginTap(BuildContext context) {
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (context) => LoginScreen()));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,13 +25,26 @@ class SignUpScreen extends StatelessWidget {
               Gaps.v80,
               Text(
                 "Sign up for TikTok",
-                style: TextStyle(fontSize: Sizes.size20, fontWeight: FontWeight.w700),
+                style: TextStyle(
+                  fontSize: Sizes.size20,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
               Gaps.v20,
               Text(
                 "Create a profile, follow other accounts, make your own videos, and more.",
                 style: TextStyle(fontSize: Sizes.size16, color: Colors.black45),
                 textAlign: TextAlign.center,
+              ),
+              Gaps.v40,
+              AuthButton(
+                icon: FaIcon(FontAwesomeIcons.user),
+                text: "Use email & password",
+              ),
+              Gaps.v16,
+              AuthButton(
+                icon: FaIcon(FontAwesomeIcons.apple),
+                text: "Continue with Apple",
               ),
             ],
           ),
@@ -32,15 +54,21 @@ class SignUpScreen extends StatelessWidget {
         color: Colors.grey.shade100,
         elevation: 2,
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: Sizes.size20),
+          padding: const EdgeInsets.symmetric(vertical: Sizes.size12),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text('Already have an account?'),
               Gaps.h5,
-              Text(
-                'Log in',
-                style: TextStyle(fontWeight: FontWeight.w600, color: Theme.of(context).primaryColor),
+              GestureDetector(
+                onTap: () => onLoginTap(context),
+                child: Text(
+                  'Log in',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ),
               ),
             ],
           ),
