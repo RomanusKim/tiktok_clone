c858846f138abbe11398005b4ac910e1090266e9
romanus <romanusdev@gmail.com>
2025-08-18 23:43:17 +0900

    #6.5 ~ #6.7

--- lib/feautres/main_navigation/main_navigation_screen.dart
+++ lib/feautres/main_navigation/main_navigation_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/feautres/main_navigation/widgets/nav_tab.dart';
+import 'package:tiktok_clone/feautres/main_navigation/widgets/stf_screen.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   MainNavigationScreen({super.key});
@@ -12,38 +16,74 @@ class MainNavigationScreen extends StatefulWidget {
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int _selectedIndex = 0;
 
-  final screens = [
-    Center(child: Text('Home')),
-    Center(child: Text('Search')),
-    Center(child: Text('Home')),
-    Center(child: Text('Search')),
-    Center(child: Text('Home')),
-  ];
+  // final screens = [
+  //   // Center(child: Text('Home')),
+  //   StfScreen(key: GlobalKey()),
+  //   StfScreen(key: GlobalKey()),
+  //   // Center(child: Text('Discover')),
+  //   Container(),
+  //   StfScreen(key: GlobalKey()),
+  //   StfScreen(key: GlobalKey()),
+  //   // Center(child: Text('Inbox')),
+  //   // Center(child: Text('User')),
+  // ];
 
   void _onTap(int index) {
     setState(() {
       _selectedIndex = index;
+      print(_selectedIndex);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: screens[_selectedIndex],
-      bottomNavigationBar: NavigationBar(
-        labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-        selectedIndex: _selectedIndex,
-        onDestinationSelected: _onTap,
-        destinations: [
-          NavigationDestination(
-            icon: FaIcon(FontAwesomeIcons.house, color: Colors.white),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: FaIcon(FontAwesomeIcons.magnifyingGlass, color: Colors.white),
-            label: 'Search',
-          ),
+      body: Stack(
+        children: [
+          Offstage(offstage: _selectedIndex != 0, child: StfScreen()),
+          Offstage(offstage: _selectedIndex != 1, child: StfScreen()),
+          Offstage(offstage: _selectedIndex != 3, child: StfScreen()),
+          Offstage(offstage: _selectedIndex != 4, child: StfScreen()),
         ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                text: "Home",
+                isSelected: _selectedIndex == 0,
+                icon: FontAwesomeIcons.house,
+                selectedIcon: FontAwesomeIcons.house,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                text: "Discover",
+                isSelected: _selectedIndex == 1,
+                icon: FontAwesomeIcons.compass,
+                selectedIcon: FontAwesomeIcons.solidCompass,
+                onTap: () => _onTap(1),
+              ),
+              NavTab(
+                text: "Inbox",
+                isSelected: _selectedIndex == 3,
+                icon: FontAwesomeIcons.message,
+                selectedIcon: FontAwesomeIcons.solidMessage,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                text: "Profile",
+                isSelected: _selectedIndex == 4,
+                icon: FontAwesomeIcons.user,
+                selectedIcon: FontAwesomeIcons.solidUser,
+                onTap: () => _onTap(4),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
