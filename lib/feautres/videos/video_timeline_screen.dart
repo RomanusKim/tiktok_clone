9957ab0ac6e2c6b988c3595203d770df457c6e89
romanus <romanusdev@gmail.com>
2025-08-28 00:24:19 +0900

    #End of Video Timeline

--- lib/feautres/videos/video_timeline_screen.dart
+++ lib/feautres/videos/video_timeline_screen.dart
@@ -33,7 +33,8 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
   }
 
   void _onVideoFinished() {
-    _pageController.nextPage(duration: _scrollDuration, curve: _scrollCurve);
+    return;
+    // _pageController.nextPage(duration: _scrollDuration, curve: _scrollCurve);
   }
 
   @override
@@ -42,15 +43,26 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
     super.dispose();
   }
 
+  Future<void> _onRefresh() {
+    return Future.delayed(Duration(seconds: 5));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-      controller: _pageController,
-      scrollDirection: Axis.vertical,
-      itemCount: _itemCount,
-      onPageChanged: _onPageChanged,
-      itemBuilder: (context, index) =>
-          VideoPost(onVideoFinished: _onVideoFinished, index: index),
+    return RefreshIndicator(
+      // 새로운 데이터를 받아오기 위한 새로고침으로 사용하면 좋을듯
+      onRefresh: _onRefresh,
+      displacement: 50,
+      edgeOffset: 20,
+      color: Theme.of(context).primaryColor,
+      child: PageView.builder(
+        controller: _pageController,
+        scrollDirection: Axis.vertical,
+        itemCount: _itemCount,
+        onPageChanged: _onPageChanged,
+        itemBuilder: (context, index) =>
+            VideoPost(onVideoFinished: _onVideoFinished, index: index),
+      ),
     );
   }
 }
--- lib/feautres/videos/widgets/video_post.dart
+++ lib/feautres/videos/widgets/video_post.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/feautres/videos/widgets/video_button.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
@@ -18,6 +20,8 @@ class VideoPost extends StatefulWidget {
   State<VideoPost> createState() => _VideoPostState();
 }
 
+// 애니메이션 컨트롤러가 1개이면 SingleTickerProviderStateMixin
+// 여러개이면 TickerProviderStateMixin
 class _VideoPostState extends State<VideoPost>
     with SingleTickerProviderStateMixin {
   final VideoPlayerController _videoPlayerController =
@@ -41,8 +45,9 @@ class _VideoPostState extends State<VideoPost>
     await _videoPlayerController.initialize();
     // _videoPlayerController.play();
     _videoPlayerController.setVolume(0);
-    setState(() {});
+    await _videoPlayerController.setLooping(true);
     _videoPlayerController.addListener(_onVideoChanged);
+    setState(() {});
   }
 
   @override
@@ -50,6 +55,9 @@ class _VideoPostState extends State<VideoPost>
     super.initState();
     _initVideoPlayer();
     _animationController = AnimationController(
+      // vsync는 애니메이션 재생을 도와준다.
+      // 위젯이 위젯 tree에 있을때만 Ticker를 유지해준다.
+      // Ticker를 생성하기 위해서는 SingleTickerProviderStateMixin을 사용. (성능저하를 막음, 위젯이 보일때만 생성)
       vsync: this,
       lowerBound: 1.0,
       upperBound: 1.5,
@@ -124,6 +132,51 @@ class _VideoPostState extends State<VideoPost>
                   ),
                 ),
               ),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            left: 10,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "@로마",
+                  style: TextStyle(
+                    fontSize: Sizes.size20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Gaps.v10,
+                Text(
+                  "This is Ocean!!",
+                  style: TextStyle(fontSize: Sizes.size16, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            right: 10,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  foregroundImage: NetworkImage(
+                    "https://avatars.githubusercontent.com/u/161845427?s=400&u=f9b105572570809f25bc6738a238df1a391c6721&v=4",
+                  ),
+                  child: Text("로마"),
+                ),
+                Gaps.v24,
+                VideoButton(icon: FontAwesomeIcons.solidHeart, text: "2.9M"),
+                Gaps.v24,
+                VideoButton(icon: FontAwesomeIcons.solidComment, text: "33K"),
+                Gaps.v24,
+                VideoButton(icon: FontAwesomeIcons.share, text: "Share"),
+              ],
             ),
           ),
         ],
